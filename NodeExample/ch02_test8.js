f14db9379801e548d5cf97cee7b3457c88385286
pollutedlake <psh950827@naver.com>
2019-03-06 22:39:37 +0900

    Use path module

--- NodeExample/ch02_test8.js
+++ NodeExample/ch02_test8.js
@@ -2,10 +2,17 @@ var path = require('path');
 
 var directories = ['Users', 'Psh95', 'docs'];
 var dirStr = directories.join();
-console.log('dir : ' + dirStr);
+console.log('dir : ' + dirStr); // dir : Users,Psh95,docs
 
 var dirStr2 = directories.join(path.sep);
-console.log('dir2 : ' + dirStr2);
+console.log('dir2 : ' + dirStr2);   // dir2 : Users\Psh95\docs
 
-var dirStr3 = path.join('/Users/Psh95', 'notepad.exe');
-console.log('dir3 : ' + dirStr3);
+var filepath = path.join('/Users/Psh95', 'notepad.exe');
+console.log('dir3 : ' + filepath); // dir3 : \Users\Psh95\notepad.exe
+
+var dirname = path.dirname(filepath);
+console.log('dirname : ' + dirname); // dirname : \Users\Psh95
+var basename = path.basename(filepath);
+console.log('basename : ' + basename); // basename : notepad.exe
+var extname = path.extname(filepath);
+console.log('extname : ' + extname);    // extname : .exe
