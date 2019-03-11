5a2308d3795a6f192233c74125d6a9830170bde8
pollutedlake <psh950827@naver.com>
2019-03-11 23:25:16 +0900

    Node study 21

--- NodeExample/ch05_test1.js
+++ NodeExample/ch05_test1.js
@@ -2,7 +2,8 @@ var http = require('http');
 
 var server = http.createServer();
 
+var host = '222.112.241.112';
 var port = 3000;
-server.listen(port, function() {
-    console.log('웹서버가 실행되었습니다. : ' + port);
+server.listen(port, host, 50000, function() {
+    console.log('웹서버가 실행되었습니다. -> ' + host + port);
 });
